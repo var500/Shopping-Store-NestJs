7ac6db833f6aaae142f4310236fc0c340e10cd56
var500 <varun5chodha@gmail.com>
2023-05-30 13:20:03 +0530

    fix:using Nestjs ConfigModule for env variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,14 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import dotenv from "dotenv";
+import { ConfigModule } from '@nestjs/config';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
 
+
 @Module({
-  imports: [ProductsModule, MongooseModule.forRoot(
+  imports: [ConfigModule.forRoot(),ProductsModule, MongooseModule.forRoot(
     process.env.MONGO_DB_URI
   )],
   controllers: [AppController],
